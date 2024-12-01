8dd5291e78a666800a1bc04e7117da7bc3eb4733
semtassta <semtassta@gmail.com>
2024-12-01 17:23:48 +0300

    portfolio

--- technical_writer_portfolio/js/portfolio_api.js
+++ technical_writer_portfolio/js/portfolio_api.js
@@ -1,12 +1,8 @@
-async function getResponce() {
-    // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
-    //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
-    //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
-//    let responce = await fetch("../../tmp/g06u28.shop.json")
-    let responce = await fetch("./data/portfolio.json")
-    console.log("responce:\n", responce, "\n /responce: \n")
-    let content = await responce.text()
-    console.log("await responce.text()\n", content)
+async function getResponse() {
+    let response = await fetch("https://semtassta.github.io/bowp_semenova/technical_writer_portfolio/data/portfolio.json")
+    console.log("response:\n", response, "\n /response: \n")
+    let content = await response.text()
+    console.log("await response.text()\n", content)
     content = JSON.parse(content)
     content = content.slice(0, 9)
     console.log("content.slice(0, 9)", content)
@@ -29,4 +25,4 @@ async function getResponce() {
                 `
     }
 }
-getResponce()
+getResponse()
